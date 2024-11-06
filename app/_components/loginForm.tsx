e64f5c730f156fa85f42f36bd67e3ac7bc471b1e
Israel Wong <israelwong@iMac-de-Israel.local>
2024-11-05 19:24:38 -0600

    web stable v1.0

---
+++ app/_components/loginForm.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LoginForm from '../login/page'
+
+function loginForm() {
+    return (
+        <div>
+            <LoginForm />
+        </div>
+    )
+}
+
+export default loginForm
