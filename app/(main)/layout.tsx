c67cce3d075c383c03304faffdab3e504a15fc73
Israel Wong <israelwong@iMac-de-Israel.local>
2025-02-16 18:40:36 -0600

    chatflow

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,10 +1,15 @@
 import { ReactNode } from "react";
 import Navbar from "./_components/Navbar";
 import Footer from "./_components/Footer";
+import Script from "next/script";
 
 export default function WebLayout({ children }: { children: ReactNode }) {
   return (
     <div className="flex flex-col">
+
+
+      <Script type="text/javascript" id="hs-script-loader" async defer src="//js-na1.hs-scripts.com/49235722.js"></Script>
+
       <Navbar />
       <main className="flex-1">{children}</main>
       <Footer />
