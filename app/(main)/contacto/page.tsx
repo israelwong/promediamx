4cb95783517f91e311515914d6e301b2a39d8a7a
Israel Wong <israelwong@iMac-de-Israel.local>
2025-02-17 13:21:56 -0600

    hs

--- app/(main)/contacto/page.tsx
+++ app/(main)/contacto/page.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import LeadForm from './_components/LeadForm'
+// import React from 'react'
+// // import LeadForm from './_components/LeadForm'
 
-export default function pages() {
-    return <LeadForm />
-}
+// export default function pages() {
+//     return <LeadForm />
+// }
